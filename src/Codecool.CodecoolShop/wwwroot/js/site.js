6cf9f2ff93c43638b749a1927e2e27dccbec8303
nagyD88 <oliverdikker@gmail.com>
2022-09-06 11:58:44 +0200

    find the "chekout" button

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const { Button } = require("../lib/bootstrap/dist/js/bootstrap")
 
-// Write your JavaScript code.
+chekout = {
+    button: function () {
+        let chekoutButton = document.getElementById("chekout");
+        chekoutButton.addEventListener('click',);
+    }
+}
