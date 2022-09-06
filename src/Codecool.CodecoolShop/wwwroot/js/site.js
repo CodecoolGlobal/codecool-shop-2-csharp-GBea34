2235b012f89eaeb640715ef90ca39756cc8bdae7
SsdHUN <santhasoma1999@gmail.com>
2022-09-06 13:46:45 +0200

    implement: create cart button with a cart modal

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Cart Modal display 
+let cart_modal = document.getElementById("cart-modal");
+let cart_button = document.getElementById("cart-button");
+let span = document.getElementsByClassName("close")[0];
+
+cart_button.onclick = function() {
+    cart_modal.style.display = "block";
+}
+
+span.onclick = function() {
+    cart_modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == cart_modal) {
+        cart_modal.style.display = "none";
+    }
+}
