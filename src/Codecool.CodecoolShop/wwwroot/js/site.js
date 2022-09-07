4eeb49dedf5ee65c77f3bfc2437e7b008c4fe3b8
nagyD88 <oliverdikker@gmail.com>
2022-09-07 09:50:17 +0200

    const global checked bool

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -1,5 +1,6 @@
 ﻿
-chekout = {
+let checked = true;
+const checkout = {
     button: function () {
         let checkoutButton = document.getElementById("checkout");
         checkoutButton.addEventListener('click',this.checkout);
