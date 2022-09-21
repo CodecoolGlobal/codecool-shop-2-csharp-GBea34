5cd298c0905237c45d86838045c63c27af67aa55
nagyD88 <oliverdikker@gmail.com>
2022-09-21 11:55:21 +0200

    fix userDao to match with the database

--- src/Codecool.CodecoolShop/Daos/Implementations/UserDao.cs
+++ src/Codecool.CodecoolShop/Daos/Implementations/UserDao.cs
@@ -6,7 +6,7 @@ namespace Codecool.CodecoolShop.Daos.Implementations
 {
     public class UserDao
     {
-
+        private string connectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=shopDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
         public bool IsUserByEmail(User user)
         {
             bool success = false;
@@ -40,13 +40,13 @@ namespace Codecool.CodecoolShop.Daos.Implementations
             if (!IsUserByEmail(user))
             {
                 string SQLstatment =
-                    "Insert Into dbo.Users (EMAIL, PASSWORD, ADMIN) values (@email, @password, @admin);";
+                    "Insert Into dbo.ShopUsers (EMAIL, PASSWORD, Name) values (@email, @password, @name);";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     SqlCommand command = new SqlCommand(SQLstatment, connection);
                     command.Parameters.Add("@email", System.Data.SqlDbType.VarChar, 100).Value = user.Email;
                     command.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 40).Value = user.Password;
-                    command.Parameters.Add("@admin", System.Data.SqlDbType.Bit).Value = user.Admin;
+                    command.Parameters.Add("@name", System.Data.SqlDbType.Bit).Value = user.Name;
 
 
                     try
