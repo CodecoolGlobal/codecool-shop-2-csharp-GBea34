013db2a1dcc6831395d7f57a7ee1836bc5fb268b
nagyD88 <oliverdikker@gmail.com>
2022-09-21 11:42:17 +0200

    fix typo

--- src/Codecool.CodecoolShop/Controllers/DataController.cs
+++ src/Codecool.CodecoolShop/Controllers/DataController.cs
@@ -1,15 +1,16 @@
-﻿using Codecool.CodecoolShop.Models;
+﻿using Codecool.CodecoolShop.Daos.Implementations;
+using Codecool.CodecoolShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Codecool.CodecoolShop.Controllers
 {
     public class DataController : Controller
     {
-        [Route ("/registration/{name}/{emial}/{password}")]
+        [Route ("/registration/{name}/{email}/{password}")]
         public string registration(string name, string email, string password)
         {
             User user = new User { Name = name,  Email = email, Password = password};
-            UsersDAO usersDAO = new UsersDAO();
+            UserDao usersDAO = new UserDao();
             bool success = usersDAO.RegisterNewUser(user);
             string serializeObject = Newtonsoft.Json.JsonConvert.SerializeObject(success);
             return serializeObject;
