5d9f3f42b79ffacb59f5c53c93923bff732466ff
nagyD88 <oliverdikker@gmail.com>
2022-09-22 18:20:45 +0200

    create showLoggedIn

--- src/Codecool.CodecoolShop/wwwroot/js/UserActivity.js
+++ src/Codecool.CodecoolShop/wwwroot/js/UserActivity.js
@@ -1,29 +1,56 @@
 ﻿import {apiPost} from "./Datahandler.js";
 
+
+ function showLoggedIn(email) {
+    apiPost("Data/GetNameByEmail", {email: email}).then(data => {
+        window.sessionStorage.setItem("name", data["name"]);
+        Exit();
+        let login = document.querySelector("#logNow");
+        login.innerText = `${data["Name"]} Logout`;
+    })
+
+
+}
+function Exit() {
+    document.getElementById('back').style.display = 'none';
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('signup').style.display = 'none';
+}
 export const userActivity = {
-    registration :function () {
+    registration: function () {
         let name = document.querySelector("#registrationName").value;
-        let password1= document.querySelector("#password1").value;
-        let password2= document.querySelector("#password2").value;
-        let email= document.querySelector("#registrationEmail").value;
-        if (password1===password2){
-            apiPost("Data/Registration", {name:name,  email:email ,password:password1}).then(data=>console.log(data))
+        let password1 = document.querySelector("#password1").value;
+        let password2 = document.querySelector("#password2").value;
+        let email = document.querySelector("#registrationEmail").value;
+        if (password1 === password2) {
+            apiPost("Data/Registration", {
+                name: name,
+                email: email,
+                password: password1
+            }).then(data => console.log(data))
+        } else {
+            prompt("nemnyert")
         }
-        else{
-            prompt("nemnyert")}
-            
+
     },
-    regButton: function (){
-        let regButton=document.querySelector("#signUpButton")
+    regButton: function () {
+        let regButton = document.querySelector("#signUpButton")
         regButton.addEventListener("click", this.registration)
     },
-    login: function(){
+    login: function () {
         let email = document.querySelector("#loginEmail").value;
         let password = document.querySelector("#loginPassword").value;
-        apiPost("Data/Login", {email:email,password:password}).then(data=>console.log(data))
+        apiPost("Data/Login", {email: email, password: password}).then(data => {
+            console.log(data)
+            if (data) {
+                showLoggedIn(email);
+            }
+        })
     },
-    loginbutton: function (){
+    loginbutton: function () {
         let logButton = document.querySelector("#loginButton");
-        logButton.addEventListener("click",this.login )
+        logButton.addEventListener("click", this.login)
     }
+
+    
 }
