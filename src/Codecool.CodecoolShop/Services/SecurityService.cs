e3b63e8396c23d87199c95d215abc16f72e98f17
nagyD88 <oliverdikker@gmail.com>
2022-09-22 16:56:38 +0200

    create SecurityService.cs

---
+++ src/Codecool.CodecoolShop/Services/SecurityService.cs
@@ -0,0 +1,14 @@
+﻿using Codecool.CodecoolShop.Daos.Implementations;
+using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.Services;
+
+public class SecurityService
+{
+    UserDao userDao = new UserDao();
+      
+    public bool IsValid(User user) 
+    {
+        return userDao.IsUserByEmailAndPassword(user);
+    }
+}
