9f50a58fe12e4e02f1e9c94e8d01145c73982767
nagyD88 <oliverdikker@gmail.com>
2022-09-22 09:23:56 +0200

    dataControl dont check the password equality

--- src/Codecool.CodecoolShop/Controllers/DataController.cs
+++ src/Codecool.CodecoolShop/Controllers/DataController.cs
@@ -7,18 +7,17 @@ namespace Codecool.CodecoolShop.Controllers
     public class DataController : Controller
     {
         [HttpPost]
-        public string registration(string name, string email, string passwordOne, string passwordTwo)
+        public string registration(string name, string email, string password)
         {
-            if (passwordOne == passwordTwo)
-            {
-                string password = passwordOne;
+            
+            
+                
                 User user = new User { Name = name,  Email = email, Password = password};
                 UserDao usersDAO = new UserDao();
                 bool success = usersDAO.RegisterNewUser(user);
                 string successed = Newtonsoft.Json.JsonConvert.SerializeObject(success);
                 return successed;
-            }
-            return "";
+            
         }
     }
 }
